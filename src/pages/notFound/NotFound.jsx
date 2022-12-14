a94120e7574a277c9cb3b00a4b4e5ad08f007b45
ciijay99 <chigozieahanwa99@gmail.com>
2022-12-14 20:50:47 +0100

    Another Commit

--- src/pages/notFound/NotFound.jsx
+++ src/pages/notFound/NotFound.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import './notFound.css'
+import {Link} from 'react-router-dom'
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
+    <section>
+        <div className="container notfound__container">
+            <h1>Page Not Found</h1>
+            <Link to="/" className='btn'>Return To HomePage</Link>
+        </div>
+    </section>
   )
 }
 
